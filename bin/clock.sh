4a53d40b5e1c23307da277f8b48b128bb87abfa2
cheezgi <zackh@firemail.cc>
2017-02-25 15:47:21 -0500

    do some notification stuff I fhink? not suer

--- bin/clock.sh
+++ bin/clock.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# get current window ID
-cw=$(pfw)
-
 # show tty-clock for 2 seconds and close
-xterm -rv -geometry 38x10 +sb -e bash -c 'w=$(pfw); tty-clock -nt & waitron window_snap bottomright; sleep 2; killw "$w"'
+xterm -rv -geometry 38x10 +sb -e bash -c 'tty-clock -nt & waitron window_snap bottomright; sleep 2'
 
-# re-focus last window
-wtf "$cw"
+#~/bin/popup.sh "$(date +"%l %_M %p")"
 
