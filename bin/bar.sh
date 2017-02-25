840e186e74193a22a4e317efbadf74a1c74e15ab
cheezgi <zackh@firemail.cc>
2017-02-25 11:49:03 -0500

    add some comments n stuff

--- bin/bar.sh
+++ bin/bar.sh
@@ -1,2 +1,3 @@
+# date and time bars
 ~/bin/bar_date.sh | ~/source/bar/lemonbar -g 190x++ -B '#4265f4' &
 ~/bin/bar_time.sh | ~/source/bar/lemonbar -g 90x+1510+  -B '#4265f4' &
--- bin/notifications.sh
+++ bin/notifications.sh
@@ -1,14 +1,22 @@
 #!/bin/zsh
 
-duration=3
+# how long bar stays open
+duration=4
 
-barx=200
-barw=400
-
+# color of bar
 bar_bg='#4265f4'
 
+# bar position
+bar_x='200'
+bar_y='0'
+
+# sind: https://github.com/htylo/simplenotifydaemon
+# custom format: appname:summary:description
+# the default format is kind of weird
 sind -f "%a:%s:%b" | while IFS=: read app sum desc; do
+    # sometimes sind emits an empty line
     if [ "$app" != " " ]; then
+        # message to send to bar
         message="$app"
         if [ "$sum" != " " ] && [ -n "$sum" ]; then
             message="$message - $sum"
@@ -16,8 +24,14 @@ sind -f "%a:%s:%b" | while IFS=: read app sum desc; do
         if [ "$desc" != " " ] && [ -n "$desc" ]; then
             message="$message - $desc"
         fi
+        # txtw: https://github.com/baskerville/txtw
+        # somewhat janky with default x font
+        # width of message for bar dimensions
         width=$(txtw $message)
-        geom="${width}x+200+"
+        # set up bar geometry
+        # <width>x<height>+<x>+<y>
+        # use default height of whatever
+        geom="${width}x+${bar_x}+${bar_y}"
         ( echo "%{c}$message"; sleep $duration ) | ~/source/bar/lemonbar -g $geom -B $bar_bg
     fi
 done
